5d9139267d542046f4c61c65d572f923e463ee39
disha <dishagsuvarna@gmail.com>
2026-02-12 13:15:44 +0530

    Initial Django project setup

---
+++ myproject/urls.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from django.urls import path
+from core.views import product_list
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('products/', product_list),
+]
